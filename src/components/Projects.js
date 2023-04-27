11d3db078a37eef5ef67da04a5fe0ce1d3bb357c
Abhishek Sharma <abhisheksaroch2505@gmail.com>
2023-04-27 08:29:29 +0530

    Did Some Changes In projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,4 @@
 import React from "react";
-import cartoon from "../images/cartoon.jpg";
 import project1 from"../images/project1.png"
 import project2 from"../images/project2.png"
 import project3 from"../images/project3.png"
